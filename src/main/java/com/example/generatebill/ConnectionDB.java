7bcd68f7f56f5220925241f8a519bd7fcb5a81d4
MelissaAyllon <mayllon@espol.edu.ec>
2023-03-15 11:57:20 -0400

    Se arreglo jdbc con el module-infojava

---
+++ src/main/java/com/example/generatebill/ConnectionDB.java
@@ -0,0 +1,6 @@
+package com.example.generatebill;
+
+import java.sql.Connection;
+public class ConnectionDB {
+    Connection connection;
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.generatebill {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.generatebill to javafx.fxml;
